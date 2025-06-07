4240817fc1c52f2806c716d60d761608bc0b35b6
radanpro <abdulrahmanraadan@gmail.com>
2025-06-07 23:04:06 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
+
+const todos = ref([]);
+const name = ref("");
+
+const input_content = ref("");
+const input_category = ref(null);
+
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  })
+);
+
+const addTodo = () => {};
+
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+});
+</script>
+
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input
+          type="text"
+          placeholder="e.g. make a video"
+          v-model="input_content"
+        />
+        <h4>Pick a category</h4>
+        <div class="options">
+          <label>
+            <input
+              v-model="input_category"
+              type="radio"
+              name="category"
+              value="business"
+            />
+            <span class="bubble business">Business</span>
+          </label>
+
+          <label>
+            <input
+              v-model="input_category"
+              type="radio"
+              name="category"
+              value="personal"
+            />
+            <span class="bubble personal">Personal</span>
+          </label>
+        </div>
+      </form>
+    </section>
+  </main>
+</template>
